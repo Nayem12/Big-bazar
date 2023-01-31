5b5cff6ef70c36d0ebd5f3bc2bdc102c502fda1e
Nayem12 <shanaeem773@gmail.com>
2023-01-31 20:34:20 +0600

    git added

--- src/Components/Pages/AllProducts/AllProduct.js
+++ src/Components/Pages/AllProducts/AllProduct.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
+import { BsFillCartPlusFill } from 'react-icons/bs';
 import { FaArrowRight } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../AuthContexts/Contexts/AuthProvider';
 import './Allproduct.css'
 
 const AllProduct = ({ allproduct }) => {
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
     const { category,
         image,
         description,
@@ -18,6 +24,29 @@ const AllProduct = ({ allproduct }) => {
         postdate,
         sellername, resolution, verification, originalprice
     } = allproduct
+    const buyeremail = user?.email;
+    const productId = allproduct?._id;
+    const newProducts = { ...allproduct, buyeremail, productId }
+    const handleAddToWishList = () => {
+        if (user?.email) {
+            fetch(`https://big-bazar-server.vercel.app/addtowishlist`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newProducts)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    toast.success('Successfully added to Wishlist')
+                })
+        }
+        else {
+            navigate("/login")
+        }
+
+
+    }
     return (
         <div>
             <div className="card card-side bg-gray-600 shadow-xl producteffect hover:bg-[#2e815d] cursor-pointer">
@@ -26,8 +55,10 @@ const AllProduct = ({ allproduct }) => {
                     <h2 className="card-title">{title}</h2>
                     <p>Click the button to watch on Jetflix app.</p>
                     <div className="card-actions justify-start">
-                        <button type="submit" className=" flex items-center gap-2 mt-2 py-2 rounded-full text-black text-lg gradiant border-0 px-10">
-                            <span>See Items</span> <FaArrowRight />
+                        <button className=" flex items-center gap-2 mt-2 py-2 rounded-full text-black text-lg gradiant border-0 px-10"
+                            onClick={() => handleAddToWishList(allproduct)}>
+                            <BsFillCartPlusFill className='text-2xl mr-2' />
+                            WishList
                         </button>
                     </div>
                 </div>
--- src/Components/Pages/Homes/Slider/Sliders.js
+++ src/Components/Pages/Homes/Slider/Sliders.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
+import { BsFillCartPlusFill } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../../AuthContexts/Contexts/AuthProvider';
 
 const Sliders = ({ allproduct }) => {
+
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
     const { category,
         image,
         description,
@@ -16,6 +23,27 @@ const Sliders = ({ allproduct }) => {
         postdate,
         sellername, resolution, verification, originalprice
     } = allproduct
+    const buyeremail = user?.email;
+    const productId = allproduct?._id;
+    const newProducts = { ...allproduct, buyeremail, productId }
+    const handleAddToWishList = () => {
+        if (user?.email) {
+            fetch(`https://big-bazar-server.vercel.app/addtowishlist`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newProducts)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    toast.success('Successfully added to Wishlist')
+                })
+        }
+        else {
+            navigate("/login")
+        }
+    }
     return (
         <div>
             <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
@@ -30,7 +58,9 @@ const Sliders = ({ allproduct }) => {
 
                     <div className="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
                         <span className="font-bold text-gray-800 dark:text-gray-200">$ {price}</span>
-                        <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button className="px-5 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-lime-500 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none" onClick={() => handleAddToWishList(allproduct)}>
+                            <BsFillCartPlusFill className='text-2xl mr-2' />
+                            WishList</button>
                     </div>
                 </div>
             </div>
